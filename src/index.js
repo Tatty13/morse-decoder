7a0d5a743ab8f0736022b3ece2cbec6291b2fb93
Tatty13 <tat.mi.iva@gmail.com>
2022-07-08 01:03:34 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let symb = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+  }
+  
+  let sentenceInNum = [];
+  
+  for (let i = 0; i < expr.length; i += 10) {
+    let dec = expr.slice(i, i + 10);
+    if (dec == '**********') {
+      dec = ' ';
+    }
+    let letterInNum = [];
+    for (let i = 0; i < 10; i += 2) {
+      letterInNum.push(dec.slice(i, i +2));
+    }
+    sentenceInNum.push(letterInNum);
+  }
+  
+  let morseSentence = [];
+
+  for (let item of sentenceInNum) {
+    let morseLetter = '';
+    item.forEach(el => el in symb ? morseLetter += symb[el] : morseLetter += '');
+    morseSentence.push(morseLetter);
+  }
+
+  let sentence = '';
+  morseSentence.forEach(el => el in MORSE_TABLE ? sentence += MORSE_TABLE[el] : sentence += ' ');
+  
+  return sentence;
 }
 
 module.exports = {
